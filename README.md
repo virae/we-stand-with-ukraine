3cc04736ec184fda7522e40ed7b52922932181eb
Michal Šimonfy <michal.simonfy@gmail.com>
2022-03-03 12:15:25 +0100

    Badge customization

--- README.md
+++ README.md
@@ -4,7 +4,37 @@ A simple website badge to show your support for Ukraine.
 
 ### Installation
 ```
-<script src="https://cdn.jsdelivr.net/gh/virae/we-stand-with-ukraine@v1.0.0/badge.js" async></script>
+<script src="https://cdn.jsdelivr.net/gh/virae/we-stand-with-ukraine@master/badge.js" async></script>
+```
+
+### Parameters
+
+You can customize the badge with additional parameters by setting script attributes:
+
+- ```title```
+
+    (string) The tooltip text to display on badge hover
+
+    Default: ```'We stand with Ukraine'```
+
+    ----
+
+- ```url```
+
+    (string) The website URL to launch when the badge is clicked
+
+    Default: nothing
+
+    ----
+
+- ```position```
+
+    (string) The position in which the badge should be displayed
+
+    Default: ```'left'```
+
+```
+<script src="https://cdn.jsdelivr.net/gh/virae/we-stand-with-ukraine@master/badge.js" title="Help Urkaine Win" url="https://example.com" position="right" async></script>
 ```
 
 ### Preview
--- badge.js
+++ badge.js
@@ -1,16 +1,45 @@
+var SWU__currentScript = document.currentScript || {};
 var SWU__DOMReady = function(callback) {
   document.readyState === 'interactive' || document.readyState === 'complete' ? callback() : document.addEventListener('DOMContentLoaded', callback);
 };
 SWU__DOMReady(function() {
-  const el = document.createElement('div')
-  el.title = 'We stand with Ukraine'
-  el.style.position = 'fixed'
-  el.style.left = '-80px'
-  el.style.bottom = '20px'
+  const el = document.createElement('div');
+
+  // Options
+  const url = SWU__currentScript.getAttribute('url');
+  const title = SWU__currentScript.getAttribute('title');
+  const position = SWU__currentScript.getAttribute('position');
+
+  // Set custom title
+  el.title = title || 'We stand with Ukraine';
+
+  // Set custom URL
+  if (url) {
+    el.style.cursor = 'pointer'
+    el.target = '_blank'
+    el.addEventListener('click', function() {
+      window.open(url, '_blank')
+    })
+  }
+
+  // Set custom position (left, right)
+  if (position === 'right') {
+    el.style.right = '-80px'
+    el.style.bottom = '20px'
+    el.style.transform = 'rotate(135deg)'
+    el.style.background = 'linear-gradient(-360deg, #005BBB 50%, #FFD500 50%)'
+  } else {
+    el.style.left = '-80px'
+    el.style.bottom = '20px'
+    el.style.transform = 'rotate(45deg)'
+    el.style.background = 'linear-gradient(-180deg, #005BBB 50%, #FFD500 50%)'
+  }
+
   el.style.width = '300px'
   el.style.height = '84px'
-  el.style.transform = 'rotate(45deg)'
-  el.style.zIndex = '999'
-  el.style.background = 'linear-gradient(-180deg, #005BBB 50%, #FFD500 50%)'
-  document.body.appendChild(el)
+  el.style.position = 'fixed';
+  el.style.zIndex = '999';
+
+  el.setAttribute('id', 'we-stand-with-ukraine')
+  document.body.appendChild(el);
 });
